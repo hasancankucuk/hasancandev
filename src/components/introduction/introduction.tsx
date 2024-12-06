d4b3056aa3f317ec1836b62686f1bd88c6ae240e
Hasan Can <hasancankucuk@yandex.com>
2024-12-06 12:55:33 +0100

    Update introduction.tsx

--- src/components/introduction/introduction.tsx
+++ src/components/introduction/introduction.tsx
@@ -1,13 +1,12 @@
 import Card from "../card/card";
 import { ProjectsCard } from "../../shared/projects/projectsCard";
 import { ProjectTypes } from "../../models/projectType";
-import { Fragment } from "react/jsx-runtime";
 
 export default function Introduction() {
     return (
         <div data-testid='introduction'>
             <Card testId="introduction" color="bg-light-MainTheme dark:bg-dark-MainTheme" margin="mx-auto my-2 " maxWidth="max-md:max-w-[90%] md:max-w-[40%] lg:max-w-[40%] max-sm:max-w-[90%]">
-                <a className="ml-5"  href="https://calendly.com/hasancankucuk/discovery-call-to-know-me-better" target="_blank" rel="noopener noreferrer">
+                <a className="ml-5" href="https://calendly.com/hasancankucuk/discovery-call-to-know-me-better" target="_blank" rel="noopener noreferrer">
                     <button className="bg-light-inlineTheme bg-green-600/[0.3] text-white py-1 px-2 rounded-lg mr-9 mt-10 float-end w-32">
                         Calendly
                     </button>
@@ -18,6 +17,9 @@ export default function Introduction() {
                             Hasan Can Küçük
                         </h4>
                         <p className=" text-black dark:text-white block font-sans text-base antialiased font-medium leading-relaxed bg-clip-text bg-gradient-to-tr from-blue-gray-600 to-blue-gray-400">
+                            📍 Now settled in Berlin, with my bags unpacked and ready to embark on my next professional journey.
+                        </p>
+                        <p className=" line-through text-black dark:text-white block font-sans text-base antialiased font-medium leading-relaxed bg-clip-text bg-gradient-to-tr from-blue-gray-600 to-blue-gray-400">
                             📍 Currently in Turkey, but my bags are packed and my passport is doing warm-up stretches.
                         </p>
                     </div>
